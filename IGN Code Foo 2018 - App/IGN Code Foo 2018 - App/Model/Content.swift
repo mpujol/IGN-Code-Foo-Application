c15a5425efcd410f6e0f225fa7ec2f6b28152d19
Michael Andrew Pujol <mike.pujol88@gmail.com>
2018-03-24 12:43:48 -0700

    Added the Content Model

---
+++ IGN Code Foo 2018 - App/IGN Code Foo 2018 - App/Model/Content.swift
@@ -0,0 +1,42 @@
+//
+//  Content.swift
+//  IGN Code Foo 2018 - App
+//
+//  Created by Michael Pujol on 3/24/18.
+//  Copyright © 2018 Michael Pujol. All rights reserved.
+//
+
+import UIKit
+
+struct Feed: Decodable {
+    let count: Int
+    let startIndex: Int
+    let data: [Data]
+    
+}
+
+
+struct Data: Decodable {
+    let contentId:String
+    let thumbnails: [Thumbnail]
+    let metadata: Metadata
+    let tags: [String]
+}
+
+struct Metadata: Decodable {
+    let contentType: String
+    let title: String
+    let description: String
+    let publishDate: String
+    let slug: String
+    let networks: [String]
+    let state: String
+    
+}
+
+struct Thumbnail: Decodable {
+    let url:String
+    let size: String
+    let width: Int
+    let height: Int
+}
